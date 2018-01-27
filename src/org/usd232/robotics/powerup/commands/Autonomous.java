003ca7b2ab9177859d0c53ff9e1e85aeeb32dba8
Brian Parks <mchosting1062@gmail.com>
2018-01-27 13:35:09 -0600

    Because KyDaddy said to

--- src/org/usd232/robotics/powerup/commands/Autonomous.java
+++ src/org/usd232/robotics/powerup/commands/Autonomous.java
@@ -22,8 +22,8 @@ public class Autonomous extends CommandGroup {
     public Autonomous() {
         BezierCurve curve = new BezierCurve();
         curve.addControlPoint(new Point(0.25, 0.25));
+        curve.addControlPoint(new Point(0.5, 0.75));
         curve.addControlPoint(new Point(0.25, 0.75));
-        curve.addControlPoint(new Point(0.75, 0.75));
         addSequential(new BezierDrive(curve, 2 * 12 * 8, 2 * 12 *8, 18, 0.5));
     }
 }
