6f939ea574bb01c17a0d692cdce2c8277c18d5c6
BrianParks1928 <mchosting1062@gmail.com>
2018-01-30 17:44:54 -0600

    Framework for hardcoded routes

--- src/org/usd232/robotics/powerup/RobotMap.java
+++ src/org/usd232/robotics/powerup/RobotMap.java
@@ -187,4 +187,21 @@ public interface RobotMap {
     public enum CalibrationMode {
         Calibrating, NotCalibrating
     }
+    /**
+     * enum constants for what our alliance is
+     * 
+     * @since 2017
+     * @version 2018
+     */
+    public enum Alliance {
+        Red, Blue
+    }    /**
+     * enum constants for robot starting position
+     * 
+     * @since 2018
+     * @version 2018
+     */
+    public enum StartingPosition {
+        Left, Right, Middle
+    }
 }
--- src/org/usd232/robotics/powerup/commands/Autonomous.java
+++ src/org/usd232/robotics/powerup/commands/Autonomous.java
@@ -1,24 +1,68 @@
 package org.usd232.robotics.powerup.commands;
 
+import org.usd232.robotics.powerup.Robot;
+import org.usd232.robotics.powerup.RobotMap.Alliance;
+import org.usd232.robotics.powerup.RobotMap.StartingPosition;
 import org.usd232.robotics.powerup.subsystems.DriveSubsystem;
+
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 /**
  * The Code to run during the autonomous section of a match.
  * 
- * @author Brian, Nathan, Cody
+ * @author Brian, Cody
  * @since Always
  * @version 2018
  */
 public class Autonomous extends CommandGroup {
-    /**
-     * This is what runs when you call autonomous
-     * 
-     * @since Always
-     * @version 2018
-     */
-    @SuppressWarnings("unused")
-    public Autonomous() {
-        DriveSubsystem ds = CommandBase.driveSubsystem;
-    }
+	/**
+	 * This is what runs when you call autonomous
+	 * 
+	 * @since Always
+	 * @version 2018
+	 */
+	@SuppressWarnings("unused")
+	public Autonomous() {
+		DriveSubsystem ds = CommandBase.driveSubsystem;
+		StartingPosition pos = (StartingPosition) Robot.positionChooser.getSelected();
+		Alliance alliance = (Alliance) Robot.allianceChooser.getSelected();
+		String gameData = DriverStation.getInstance().getGameSpecificMessage();
+		char[] sides = gameData.toCharArray();
+		if (alliance.equals(Alliance.Blue)) {
+			if (sides[0] == 'L') {
+				if (sides[0] == 'L') {
+
+				}
+				if (sides[0] == 'R') {
+
+				}
+			}
+			if (sides[0] == 'R') {
+				if (sides[0] == 'L') {
+
+				}
+				if (sides[0] == 'R') {
+
+				}
+			}
+		} else if(alliance.equals(Alliance.Red)) {
+			if (sides[0] == 'L') {
+				if (sides[0] == 'L') {
+
+				}
+				if (sides[0] == 'R') {
+
+				}
+			}
+			if (sides[0] == 'R') {
+				if (sides[0] == 'L') {
+
+				}
+				if (sides[0] == 'R') {
+
+				}
+			}
+		}
+	}
 }
