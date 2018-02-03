1d4b04d76f9029a94cf001aa8783f364edeec092
Zach Deibert <zachdeibert@gmail.com>
2018-02-03 11:47:58 -0600

    Added Logger class

--- src/org/usd232/robotics/powerup/log/LogServer.java
+++ src/org/usd232/robotics/powerup/log/LogServer.java
@@ -6,15 +6,24 @@ import java.io.UnsupportedEncodingException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import javax.net.ServerSocketFactory;
 
 public class LogServer implements Runnable {
-    private static final int PORT_NUM   = 5800;
-    private List<Socket>     socketList = new LinkedList<Socket>();
+    private static final Logger          LOG        = new Logger();
+    private static final int             PORT_NUM   = 5800;
+    private final List<Socket>           socketList = new LinkedList<Socket>();
+    private static final List<LogServer> servers    = new ArrayList<LogServer>();
 
-    public void write(byte[] b, int off, int len, long date, LogLevel level, String logger) throws IOException {
+    static void emit(byte[] buffer) throws IOException {
+        for (LogServer server : servers) {
+            server.write(buffer);
+        }
+    }
+    
+    static byte[] serialize(byte[] b, int off, int len, long date, LogLevel level, String logger) throws IOException {
         byte[] loggerBytes = logger.getBytes("UTF-8");
         byte[] buffer = new byte[4 + len + 8 + 1 + 4 + loggerBytes.length];
         ByteBuffer buf = ByteBuffer.wrap(buffer);
@@ -24,6 +33,10 @@ public class LogServer implements Runnable {
         buf.put((byte) level.getLevelId());
         buf.putInt(loggerBytes.length);
         buf.put(loggerBytes);
+        return buffer;
+    }
+
+    public void write(byte[] buffer) throws IOException {
         for (Socket socket : socketList) {
             socket.getOutputStream().write(buffer);
             socket.getOutputStream().flush();
@@ -52,17 +65,21 @@ public class LogServer implements Runnable {
             System.err.println("Unable to create server");
             System.exit(-1);
         }
-        System.out.printf("LogServer running on port: %s%n", PORT_NUM);
+        LOG.info("LogServer running on port %s", PORT_NUM);
         while (true) {
             Socket socket = null;
             try {
                 socket = serverSocket.accept();
                 socketList.add(socket);
-                System.out.println("Socket at " + socket.getInetAddress() + " has successfully connected.");
+                LOG.info("Socket at %s has successfully connected.", socket.getInetAddress());
             } catch (IOException ex) {
                 ex.printStackTrace();
             }
         }
+    }
+    
+    public LogServer() {
+        servers.add(this);
     }
 
     public static void main(String[] args) {
