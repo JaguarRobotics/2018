69be97ff9db061f84acbe04e8f7aded99aa88fea
Brian Parks <mchosting1062@gmail.com>
2018-02-02 19:43:01 -0600

    Changed auto from encoder turn to gyro turn

--- src/org/usd232/robotics/powerup/commands/Autonomous.java
+++ src/org/usd232/robotics/powerup/commands/Autonomous.java
@@ -6,6 +6,7 @@ import org.usd232.robotics.powerup.RobotMap.Alliance;
 import org.usd232.robotics.powerup.RobotMap.StartingPosition;
 import org.usd232.robotics.powerup.drive.EncoderDrive;
 import org.usd232.robotics.powerup.drive.EncoderTurn;
+import org.usd232.robotics.powerup.drive.GyroTurn;
 import org.usd232.robotics.powerup.subsystems.DriveSubsystem;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.command.CommandGroup;
@@ -26,51 +27,53 @@ public class Autonomous extends CommandGroup {
      */
     @SuppressWarnings("unused")
     public Autonomous() {
-        int positiveSide = 1;// Change to -1 if wired backwards
+        double ROBOT_LENGTH = 24.5;
+        int positiveSide = -1;// Change to -1 if wired backwards
         DriveSubsystem ds = CommandBase.driveSubsystem;
         StartingPosition pos = (StartingPosition) Robot.positionChooser.getSelected();
         Alliance alliance = (Alliance) Robot.allianceChooser.getSelected();
         String gameData = DriverStation.getInstance().getGameSpecificMessage();
         char[] sides = gameData.toCharArray();
+        System.out.println(sides.length);
         if (alliance.equals(Alliance.Red)) {
             if (pos.equals(RobotMap.StartingPosition.One)) {
                 if (sides[1] == 'L') {
-                    addSequential(new EncoderDrive(300));
+                    addSequential(new EncoderDrive(300 - ROBOT_LENGTH));
                 }
                 if (sides[1] == 'R') {
-                    addSequential(new EncoderDrive(206));
-                    addSequential(new EncoderTurn(ds.getRadiansFromDegrees(positiveSide * 90)));
+                    addSequential(new EncoderDrive(206 - ROBOT_LENGTH));
+                    addSequential(new GyroTurn(90 * positiveSide));
                     addSequential(new EncoderDrive(132));
-                    addSequential(new EncoderTurn(ds.getRadiansFromDegrees(positiveSide * -90)));
+                    addSequential(new GyroTurn(90 * -positiveSide));
                     addSequential(new EncoderDrive(32));
                 }
             }
             if (pos.equals(RobotMap.StartingPosition.Two)) {
                 if (sides[1] == 'L') {
-                    addSequential(new EncoderDrive(60));
-                    addSequential(new EncoderTurn(ds.getRadiansFromDegrees(positiveSide * -90)));
+                    addSequential(new EncoderDrive(60 - ROBOT_LENGTH));
+                    addSequential(new GyroTurn(-90 * positiveSide));
                     addSequential(new EncoderDrive(64));
-                    addSequential(new EncoderTurn(ds.getRadiansFromDegrees(positiveSide * 90)));
+                    addSequential(new GyroTurn(90 * positiveSide));
                     addSequential(new EncoderDrive(160));
                 }
                 if (sides[1] == 'R') {
-                    addSequential(new EncoderDrive(60));
-                    addSequential(new EncoderTurn(ds.getRadiansFromDegrees(positiveSide * 90)));
+                    addSequential(new EncoderDrive(60 - ROBOT_LENGTH));
+                    addSequential(new GyroTurn(90 * positiveSide));
                     addSequential(new EncoderDrive(90));
-                    addSequential(new EncoderTurn(ds.getRadiansFromDegrees(positiveSide * -90)));
+                    addSequential(new GyroTurn(-90 * positiveSide));
                     addSequential(new EncoderDrive(184));
                 }
             }
             if (pos.equals(RobotMap.StartingPosition.Three)) {
                 if (sides[1] == 'L') {
-                    addSequential(new EncoderDrive(206));
-                    addSequential(new EncoderTurn(ds.getRadiansFromDegrees(positiveSide * -90)));
+                    addSequential(new EncoderDrive(206 - ROBOT_LENGTH));
+                    addSequential(new GyroTurn(-90 * positiveSide));
                     addSequential(new EncoderDrive(132));
-                    addSequential(new EncoderTurn(ds.getRadiansFromDegrees(positiveSide * 90)));
+                    addSequential(new GyroTurn(90 * positiveSide));
                     addSequential(new EncoderDrive(32));
                 }
                 if (sides[1] == 'R') {
-                    addSequential(new EncoderDrive(300));
+                    addSequential(new EncoderDrive(300 - ROBOT_LENGTH));
                 }
             }
         }
@@ -78,42 +81,42 @@ public class Autonomous extends CommandGroup {
         if (alliance.equals(Alliance.Blue)) {
             if (pos.equals(RobotMap.StartingPosition.Three)) {
                 if (sides[1] == 'L') {
-                    addSequential(new EncoderDrive(300));
+                    addSequential(new EncoderDrive(300 - ROBOT_LENGTH));
                 }
                 if (sides[1] == 'R') {
-                    addSequential(new EncoderDrive(206));
-                    addSequential(new EncoderTurn(ds.getRadiansFromDegrees(positiveSide * 90)));
+                    addSequential(new EncoderDrive(206 - ROBOT_LENGTH));
+                    addSequential(new GyroTurn(90 * positiveSide));
                     addSequential(new EncoderDrive(132));
-                    addSequential(new EncoderTurn(ds.getRadiansFromDegrees(positiveSide * -90)));
+                    addSequential(new GyroTurn(-90 * positiveSide));
                     addSequential(new EncoderDrive(32));
                 }
             }
             if (pos.equals(RobotMap.StartingPosition.Two)) {
                 if (sides[1] == 'L') {
-                    addSequential(new EncoderDrive(60));
-                    addSequential(new EncoderTurn(ds.getRadiansFromDegrees(positiveSide * 90)));
+                    addSequential(new EncoderDrive(60 - ROBOT_LENGTH));
+                    addSequential(new GyroTurn(90 * positiveSide));
                     addSequential(new EncoderDrive(64));
-                    addSequential(new EncoderTurn(ds.getRadiansFromDegrees(positiveSide * -90)));
+                    addSequential(new GyroTurn(-90 * positiveSide));
                     addSequential(new EncoderDrive(160));
                 }
                 if (sides[1] == 'R') {
-                    addSequential(new EncoderDrive(60));
-                    addSequential(new EncoderTurn(ds.getRadiansFromDegrees(positiveSide * -90)));
+                    addSequential(new EncoderDrive(60 - ROBOT_LENGTH));
+                    addSequential(new GyroTurn(-90 * positiveSide));
                     addSequential(new EncoderDrive(90));
-                    addSequential(new EncoderTurn(ds.getRadiansFromDegrees(positiveSide * 90)));
+                    addSequential(new GyroTurn(90 * positiveSide));
                     addSequential(new EncoderDrive(184));
                 }
             }
             if (pos.equals(RobotMap.StartingPosition.One)) {
                 if (sides[1] == 'L') {
-                    addSequential(new EncoderDrive(206));
-                    addSequential(new EncoderTurn(ds.getRadiansFromDegrees(positiveSide * -90)));
+                    addSequential(new EncoderDrive(206 - ROBOT_LENGTH));
+                    addSequential(new GyroTurn(-90 * positiveSide));
                     addSequential(new EncoderDrive(132));
-                    addSequential(new EncoderTurn(ds.getRadiansFromDegrees(positiveSide * 90)));
+                    addSequential(new GyroTurn(90 * positiveSide));
                     addSequential(new EncoderDrive(32));
                 }
                 if (sides[1] == 'R') {
-                    addSequential(new EncoderDrive(300));
+                    addSequential(new EncoderDrive(300 - ROBOT_LENGTH));
                 }
             }
         }
