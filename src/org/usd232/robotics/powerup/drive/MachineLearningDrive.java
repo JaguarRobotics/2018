cee43920ed7b8257db44c21a505029e587ab0ac2
Zach Deibert <zachdeibert@gmail.com>
2018-02-03 22:35:30 -0600

    Created MachineLearningDrive

---
+++ src/org/usd232/robotics/powerup/drive/MachineLearningDrive.java
@@ -0,0 +1,84 @@
+package org.usd232.robotics.powerup.drive;
+
+import org.usd232.robotics.autonomous.Point;
+import org.usd232.robotics.powerup.commands.CommandBase;
+import org.usd232.robotics.powerup.learning.MachineLearner;
+import org.usd232.robotics.powerup.learning.Vector;
+
+public abstract class MachineLearningDrive extends CommandBase {
+    private static final double ACCURACY       = 0.001;
+    private static final double ROUGH_ACCURACY = 0.01;
+    private MachineLearner      learner;
+    private double              lastX;
+    private double              lastY;
+    private double              lastAngle;
+    private Vector              iv;
+
+    protected abstract Point calculateFormula(double t);
+
+    protected Point closestCurvePointTo(double x, double y) {
+        Point closest = null;
+        double distance = Double.MAX_VALUE;
+        for (double t = 0; t <= (1 + ROUGH_ACCURACY / 2); t += ROUGH_ACCURACY) {
+            Point pt = calculateFormula(t);
+            double dx = x - pt.getX();
+            double dy = y - pt.getY();
+            double dist = dx * dx + dy * dy;
+            if (dist < distance) {
+                closest = pt;
+                distance = dist;
+            }
+        }
+        return closest;
+    }
+
+    @Override
+    protected void initialize() {
+        learner = new MachineLearner(10);
+        learner.learn(new Vector().setComponent("angle", Double.MAX_VALUE),
+                        new Vector().setComponent("left", 1).setComponent("right", 1));
+        iv = null;
+    }
+
+    @Override
+    protected void execute() {
+        double x = locationSubsystem.getX();
+        double y = locationSubsystem.getY();
+        double angle = locationSubsystem.getAngle();
+        double dx = x - lastX;
+        double dy = y - lastY;
+        double dAngle = angle - lastAngle;
+        lastX = x;
+        lastY = y;
+        lastAngle = angle;
+        if (iv != null) {
+            learner.learn(iv, new Vector().setComponent("angle", dAngle));
+        }
+        double distanceTraveled = Math.sqrt(dx * dx + dy * dy);
+        double targetAngle = 0;
+        for (double diff = Math.PI / 2; diff > ACCURACY; diff /= 2) {
+            Point value = closestCurvePointTo(x + distanceTraveled * Math.cos(targetAngle), y + distanceTraveled * Math.sin(targetAngle));
+            double reportedAngle = Math.atan2(value.getY() - y, value.getX() - x);
+            if (reportedAngle < 0) {
+                targetAngle -= diff;
+            } else if (reportedAngle > 0) {
+                targetAngle += diff;
+            } else {
+                break;
+            }
+        }
+        iv = learner.calculate(new Vector().setComponent("angle", targetAngle));
+        driveSubsystem.driveTank(iv.getComponent("left"), iv.getComponent("right"));
+    }
+
+    @Override
+    protected void end() {
+        // Free up some memory
+        learner = null;
+    }
+
+    public MachineLearningDrive() {
+        requires(driveSubsystem);
+        requires(locationSubsystem);
+    }
+}
---
+++ src/org/usd232/robotics/powerup/drive/MachineLearningDriveStraight.java
@@ -0,0 +1,42 @@
+package org.usd232.robotics.powerup.drive;
+
+import org.usd232.robotics.autonomous.Point;
+
+public class MachineLearningDriveStraight extends MachineLearningDrive {
+    private double distance;
+    private double startX;
+    private double startY;
+    private double angle;
+
+    @Override
+    protected Point calculateFormula(double t) {
+        return new Point(startX + distance * Math.cos(angle) * t, startY + distance * Math.sin(angle) * t);
+    }
+
+    @Override
+    protected Point closestCurvePointTo(double x, double y) {
+        double dx = x - startX;
+        double dy = y - startY;
+        double distance = (Math.PI / 2 - angle + Math.atan2(dy, dx)) * Math.sqrt(dx * dx + dy * dy);
+        return new Point(startX + distance * Math.cos(angle), startY + distance * Math.sin(angle));
+    }
+
+    @Override
+    protected void initialize() {
+        startX = locationSubsystem.getX();
+        startY = locationSubsystem.getY();
+        angle = locationSubsystem.getAngle();
+    }
+
+    @Override
+    protected boolean isFinished() {
+        double x = locationSubsystem.getX() - startX;
+        double y = locationSubsystem.getY() - startY;
+        double linearDistanceTraveled = x * Math.sin(angle) + y * Math.cos(angle);
+        return linearDistanceTraveled >= distance;
+    }
+
+    public MachineLearningDriveStraight(double inches) {
+        distance = inches;
+    }
+}
