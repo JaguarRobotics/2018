dded267d6aae12428a835879be5e3e5ed167847b
BrianParks1928 <mchosting1062@gmail.com>
2018-02-05 17:46:10 -0600

    Work from desktop, doesnt work

--- generator/org/usd232/robotics/robot/autogenerator/buttons/EncoderDriveButton.java
+++ generator/org/usd232/robotics/robot/autogenerator/buttons/EncoderDriveButton.java
@@ -1,11 +1,21 @@
 package org.usd232.robotics.robot.autogenerator.buttons;
 
 import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+
 import javax.swing.JButton;
 
+import org.usd232.robotics.robot.autogenerator.Main;
+
 public class EncoderDriveButton extends JButton {
+	Point pointStart = null;
+	Point pointEnd = null;
 	/**
 	 * Generated Serial Version UID
 	 */
@@ -21,7 +31,42 @@ public class EncoderDriveButton extends JButton {
 		addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				drawLine();
 			}
 		});
 	}
-}
+
+	public void drawLine() {
+		pointStart = null;
+		pointEnd = null;
+
+		Main.screen.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				pointStart = e.getPoint();
+				System.out.println("Pressed");
+			}
+
+			public void mouseReleased(MouseEvent e) {
+//				pointStart = null;
+			}
+		});
+		Main.screen.addMouseMotionListener(new MouseMotionAdapter() {
+			public void mouseMoved(MouseEvent e) {
+				pointEnd = e.getPoint();
+			}
+
+			public void mouseDragged(MouseEvent e) {
+				pointEnd = e.getPoint();
+				Main.screen.repaint();
+			}
+		});
+	}
+
+	public void paint(Graphics g) {
+		super.paint(g);
+		if (pointStart != null) {
+			g.setColor(Color.RED);
+			g.drawLine(pointStart.x, pointStart.y, pointEnd.x, pointEnd.y);
+		}
+	}
+};
--- generator/org/usd232/robotics/robot/autogenerator/ui/Robot.java
+++ generator/org/usd232/robotics/robot/autogenerator/ui/Robot.java
@@ -1,7 +1,13 @@
 package org.usd232.robotics.robot.autogenerator.ui;
 
 import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class Robot extends JPanel {
@@ -18,3 +24,47 @@ public class Robot extends JPanel {
 	}
 }
 // Field size 54X27
+/*
+Code Found to be able to draw a line
+public static void main(String args[]) throws Exception {
+JFrame f = new JFrame("Draw a Red Line");
+f.setSize(300, 300);
+f.setLocation(300, 300);
+f.setResizable(false);
+JPanel p = new JPanel() {
+	Point pointStart = null;
+	Point pointEnd = null;
+	{
+		addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				pointStart = e.getPoint();
+			}
+
+			public void mouseReleased(MouseEvent e) {
+				pointStart = null;
+			}
+		});
+		addMouseMotionListener(new MouseMotionAdapter() {
+			public void mouseMoved(MouseEvent e) {
+				pointEnd = e.getPoint();
+			}
+
+			public void mouseDragged(MouseEvent e) {
+				pointEnd = e.getPoint();
+				repaint();
+			}
+		});
+	}
+
+	public void paint(Graphics g) {
+		super.paint(g);
+		if (pointStart != null) {
+			g.setColor(Color.RED);
+			g.drawLine(pointStart.x, pointStart.y, pointEnd.x, pointEnd.y);
+		}
+	}
+};
+f.add(p);
+f.setVisible(true);
+}
+*/
