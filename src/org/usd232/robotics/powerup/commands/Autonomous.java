11db53891bfed850e63ebea72fef29ff8ad57aec
Zach Deibert <zachdeibert@gmail.com>
2018-02-09 20:36:36 -0600

    Added delay drive code

--- src/org/usd232/robotics/powerup/commands/Autonomous.java
+++ src/org/usd232/robotics/powerup/commands/Autonomous.java
@@ -1,6 +1,9 @@
 package org.usd232.robotics.powerup.commands;
 
+import org.usd232.robotics.powerup.ISpeedFunction;
+import org.usd232.robotics.powerup.drive.Delay;
 import org.usd232.robotics.powerup.drive.DriveForward;
+import org.usd232.robotics.powerup.drive.DriveTurn;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 /**
@@ -18,8 +21,12 @@ public class Autonomous extends CommandGroup {
      * @version 2018
      */
     public Autonomous() {
-        addSequential(new DriveForward(
-                        t->Math.min(0.8, 9.323308271 * t * t * t - 18.42105263 * t * t + 8.897744361 * t + 0.6),
-                        22 * 12, 0.1, 0.01));
+        ISpeedFunction speed = t->Math.min(0.8, 9.323308271 * t * t * t - 18.42105263 * t * t + 8.897744361 * t + 0.6);
+        for (int i = 0; i < 400; ++i) {
+            addSequential(new DriveForward(speed, 48, 0.1, 0.01));
+            addSequential(new Delay(1000));
+            addSequential(new DriveTurn(speed, Math.PI / 2));
+            addSequential(new Delay(1000));
+        }
     }
 }
