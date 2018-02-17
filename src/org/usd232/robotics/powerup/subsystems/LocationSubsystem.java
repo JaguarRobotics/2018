5b3d6f076bac4e1256807c036b543f64984ae5e1
Zach Deibert <zachdeibert@gmail.com>
2018-02-17 08:24:01 -0600

    Skeletoned out the `LocationSubsystem`

--- src/org/usd232/robotics/powerup/subsystems/LocationSubsystem.java
+++ src/org/usd232/robotics/powerup/subsystems/LocationSubsystem.java
@@ -4,24 +4,13 @@ import org.usd232.robotics.powerup.log.Logger;
 import org.usd232.robotics.powerup.minimap.IMinimapCoordProvider;
 
 public class LocationSubsystem extends SubsystemBase implements IMinimapCoordProvider {
-    private static final Logger LOG            = new Logger();
-    private static final double WIDTH          = 18;
-    private static final double CENTER_OF_MASS = 0.5;
-    /**
-     * Diameter of pulleys, used for encoder calculations. (in inches)
-     */
-    private static final double DIAMETER       = 6;
-    /**
-     * pulses per rotation for the encoders.
-     */
-    private static final int    PPR            = 400 * 3;
+    private static final Logger LOG = new Logger();
     private double              angleOffset;
-    private double              lastS1;
-    private double              lastS2;
-    private double              lastTheta;
     private double              x;
     private double              y;
     private double              theta;
+    private double              velocity;
+    private long                lastTime;
 
     public LocationSubsystem() {
         reset();
@@ -45,47 +34,21 @@ public class LocationSubsystem extends SubsystemBase implements IMinimapCoordPro
 
     public void reset() {
         LOG.debug("Resetting LocationSubsystem");
-        leftDriveEncoder.setDistancePerPulse(Math.PI * DIAMETER / PPR);
-        rightDriveEncoder.setDistancePerPulse(Math.PI * DIAMETER / PPR);
-        leftDriveEncoder.reset();
-        rightDriveEncoder.reset();
         angleOffset = gyro.getAngle() * Math.PI / 180 - Math.PI / 2;
         x = 0;
         y = 0;
-        lastS1 = 0;
-        lastS2 = 0;
-        lastTheta = Math.PI / 2;
     }
 
     public void updateValues() {
-        double s1 = -leftDriveEncoder.getDistance();
-        double s2 = rightDriveEncoder.getDistance();
         theta = gyro.getAngle() * Math.PI / 180 - angleOffset;
-        double ds1 = s1 - lastS1;
-        double ds2 = s2 - lastS2;
-        double dtheta = theta - lastTheta;
-        lastS1 = s1;
-        lastS2 = s2;
-        lastTheta = theta;
-        double xPart;
-        double yPart;
-        if (dtheta == 0) {
-            xPart = (ds1 + ds2) / 2;
-            yPart = 0;
-        } else {
-            double coefficient = ((ds1 + ds2) / dtheta - WIDTH) / 2 + WIDTH * CENTER_OF_MASS;
-            xPart = -coefficient * Math.sin(dtheta);
-            yPart = coefficient * (1 - Math.cos(dtheta));
-        }
+        long time = System.currentTimeMillis();
+        double dt = ((double) (time - lastTime)) / 1000.0;
+        lastTime = time;
+        // TODO acceleration
+        double ds = velocity * dt;
         double sin = Math.sin(theta);
         double cos = Math.cos(theta);
-        x += xPart * cos + yPart * sin;
-        y += xPart * sin + yPart * cos;
-    }
-
-    /**
-     * Gets the angle based on where the encoders have gone.
-     */
-    public void getEncoderAngle() {
+        x += ds * sin;
+        y += ds * cos;
     }
 }
