de74137de3b0ce4e1d2fb465630b703acd518b48
Zach Deibert <zachdeibert@gmail.com>
2018-02-17 00:35:05 -0600

    Cleaned up client code

--- minimap/org/usd232/robotics/robot/minimap/MinimapCoordsClient.java
+++ minimap/org/usd232/robotics/robot/minimap/MinimapCoordsClient.java
@@ -3,65 +3,43 @@ package org.usd232.robotics.robot.minimap;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import org.usd232.robotics.powerup.subsystems.LocationSubsystem;
 import java.nio.ByteBuffer;
-import java.util.Arrays;
 
 public class MinimapCoordsClient {
-	
-	private static final int PORT = 5801;
-	private static double robotX = 0;
-	private static double robotY = 0;
-	private static double robotAngle = 0;
-	private static InetAddress adrs;
-	private static byte[] inbuf = new byte[24];
-	private static DatagramPacket packet;
-	private static DatagramSocket socket;
-	
-	public MinimapCoordsClient() throws Exception {
-		adrs = InetAddress.getByName("10.18.10.2");
-	    socket = new DatagramSocket();
-	    socket.connect(adrs, PORT);
-	    byte[] greeting = new byte[128];
-	    greeting = "Hello".getBytes();
-	    packet = new DatagramPacket(greeting, greeting.length, adrs, PORT);
-	    socket.send(packet);
-	}
-	
-	public static void recievePositionInfo() throws Exception {
-		packet = new DatagramPacket(inbuf, inbuf.length);
-		socket.receive(packet);
+    private static final int      PORT       = 5801;
+    private double         robotX     = 0;
+    private double         robotY     = 0;
+    private double         robotAngle = 0;
+    private DatagramSocket socket;
 
-//		for(byte i = 0; i < 8; i++) {
-//			outbuf[i] = xBytes[i];
-//		}
-//		for(byte i = 8; i < 16; i++) {
-//			outbuf[i] = yBytes[i];
-//		}
-//		for(byte i = 16; i < 24; i++) {
-//			outbuf[i] = angleBytes[i];
-//		}
+    public MinimapCoordsClient() throws Exception {
+        InetAddress adrs = InetAddress.getByName("10.18.10.2");
+        socket = new DatagramSocket();
+        socket.connect(adrs, PORT);
+        byte[] greeting = "Hello".getBytes();
+        DatagramPacket packet = new DatagramPacket(greeting, greeting.length, adrs, PORT);
+        socket.send(packet);
+    }
 
-		robotX = toDouble(Arrays.copyOfRange(inbuf, 0, 8));
-		robotY = toDouble(Arrays.copyOfRange(inbuf, 8, 16));
-		robotAngle = toDouble(Arrays.copyOfRange(inbuf, 16, 24));
-		recievePositionInfo();
-	}
+    public void recievePositionInfo() throws Exception {
+        byte[] inbuf = new byte[24];
+        DatagramPacket packet = new DatagramPacket(inbuf, inbuf.length);
+        socket.receive(packet);
+        ByteBuffer buffer = ByteBuffer.wrap(inbuf);
+        robotX = buffer.getDouble();
+        robotY = buffer.getDouble();
+        robotAngle = buffer.getDouble();
+    }
 
-	public static double toDouble(byte[] bytes) {
-	    return ByteBuffer.wrap(bytes).getDouble();
-	}
-	
-	public static double getX() {
-		return robotX;
-	}
-	
-	public static double getY() {
-		return robotY;
-	}
-	
-	public static double getAngle() {
-		return robotAngle;
-	}
+    public double getX() {
+        return robotX;
+    }
+
+    public double getY() {
+        return robotY;
+    }
+
+    public double getAngle() {
+        return robotAngle;
+    }
 }
-
