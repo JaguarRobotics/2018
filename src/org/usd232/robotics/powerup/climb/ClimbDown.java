a6d3c4b54f20aba6c659cc4004f15f6a62e0c333
FRC Team 1810 <1810programming@gmail.com>
2018-03-10 10:09:41 -0600

    Fixed Climb

--- src/org/usd232/robotics/powerup/climb/ClimbDown.java
+++ src/org/usd232/robotics/powerup/climb/ClimbDown.java
@@ -1,5 +1,7 @@
 package org.usd232.robotics.powerup.climb;
 
+import java.util.function.Supplier;
+
 import org.usd232.robotics.powerup.commands.CommandBase;
 import org.usd232.robotics.powerup.log.Logger;
 
@@ -11,63 +13,64 @@ import org.usd232.robotics.powerup.log.Logger;
  * @version 2018
  */
 public class ClimbDown extends CommandBase {
-    /**
-     * The Logger.
-     * 
-     * @since 2018
-     * @version 2018
-     */
-    private static final Logger LOG = new Logger();
-    /**
-     * The speed to climb down at.
-     * 
-     * @since 2018
-     */
-    private static double speed = 1;
-    /**
-     * Makes the robot climb down given the value of the trigger.
-     * 
-     * @since 2018
-     */
-    public ClimbDown(double axisValue) {
-    	speed = axisValue;
-    }
+	/**
+	 * The Logger.
+	 * 
+	 * @since 2018
+	 * @version 2018
+	 */
+	private static final Logger LOG = new Logger();
+	/**
+	 * The speed to climb down at.
+	 * 
+	 * @since 2018
+	 */
+	private Supplier<Double> speed;
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    protected void execute() {
-        LOG.catchAll(()-> {
-            liftSubsystem.climbDown(speed);
-        });
-    }
+	/**
+	 * Makes the robot climb down given the value of the trigger.
+	 * 
+	 * @since 2018
+	 */
+	public ClimbDown(Supplier<Double> axisValue) {
+		speed = axisValue;
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    protected void end() {
-        LOG.catchAll(()-> {
-            liftSubsystem.stopClimbing();
-        });
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void execute() {
+		LOG.catchAll(() -> {
+			liftSubsystem.climbDown(speed.get());
+		});
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    protected void interrupted() {
-        LOG.catchAll(()-> {
-            end();
-        });
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void end() {
+		LOG.catchAll(() -> {
+			liftSubsystem.stopClimbing();
+		});
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    protected boolean isFinished() {
-        return false;
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void interrupted() {
+		LOG.catchAll(() -> {
+			end();
+		});
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected boolean isFinished() {
+		return false;
+	}
 }
